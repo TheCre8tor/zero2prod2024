5bb93c861fec2a43952c232970139d463d9d2be6
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-02-24 11:19:44 +0100

    4.5.7 tracing-bunyan-formatter

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use env_logger::Env;
-use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
     // We are falling back to printing all logs at info-level or above
     // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // `set_global_default`can beused by applicationsto specify
+    // what subscriber shouldbeused toprocess spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
