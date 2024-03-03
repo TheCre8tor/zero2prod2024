bf05e0b1b416a755a613ea81fdfe8cfe2fdf3fbe
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-03-03 13:47:58 +0100

    sharing test helpers

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,12 @@ async fn main() -> Result<(), std::io::Error> {
         .sender()
         .expect("Invalid sender email address");
 
+    let timeout = configuration.email_client.timeout();
     let email_client = EmailClient::new(
         configuration.email_client.base_url,
         sender_email,
         configuration.email_client.authorization_token,
+        timeout,
     );
 
     let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -28,7 +28,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
-async fn spawn_app() -> TestApp {
+pub async fn spawn_app() -> TestApp {
     // The first time `initialize` is invoked the code in`TRACING` is executed.
     // All other invocations will instead skip execution.
     Lazy::force(&TRACING);
@@ -47,10 +47,12 @@ async fn spawn_app() -> TestApp {
         .sender()
         .expect("Invalid sender email address");
 
+    let timeout = configuration.email_client.timeout();
     let email_client = EmailClient::new(
         configuration.email_client.base_url,
         sender_email,
         configuration.email_client.authorization_token,
+        timeout,
     );
 
     configuration.database.database_name = Uuid::new_v4().to_string();
@@ -68,7 +70,7 @@ async fn spawn_app() -> TestApp {
     }
 }
 
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
     let connection = PgPoolOptions::new()
         .connect_with(config.without_db())
@@ -92,91 +94,4 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to migrate the database");
 
     connection_pool
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    // Arrange
-    let app = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    // Act
-    let endpoint = format!("{}/health_check", &app.address);
-
-    let response = client
-        .get(&endpoint)
-        .send()
-        .await
-        .expect("Failed to execute request");
-
-    // Assert
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-#[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
-    let app = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    // Act
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let endpoint = format!("{}/subscriptions", &app.address);
-    let response = client
-        .post(&endpoint)
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    // Assert
-    assert_eq!(200, response.status().as_u16());
-
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&app.db_pool)
-        .await
-        .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
-    assert_eq!(saved.name, "le guin");
-}
-
-#[tokio::test]
-async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
-    // Arrange
-    let app = spawn_app().await;
-    let client = reqwest::Client::new();
-    let test_cases = vec![
-        ("name=le%20guin", "missing the email"),
-        ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("name=le%20guin&email=", "invalid email"),
-        ("", "missing both name and email"),
-        (
-            "name=alexander{}%&email=ursula_le_guin%40gmail.com",
-            "invalid subscriber name",
-        ),
-    ];
-
-    for (invalid_body, error_message) in test_cases {
-        // Act
-        let endpoint = format!("{}/subscriptions", app.address);
-        let response = client
-            .post(&endpoint)
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request");
-
-        // Assert
-        assert_eq!(
-            400,
-            response.status().as_u16(),
-            // Additional customised error message on test failure
-            "The API did not fail with 400 Bad Request when the payload was {}.",
-            error_message
-        )
-    }
 }
