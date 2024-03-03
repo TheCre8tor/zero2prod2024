c9de038c828bbdd8f5a4b9df28aea33ac5527237
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-03-03 14:48:16 +0100

    testing hooks in our startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
-use zero2prod::email_client::EmailClient;
-
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -14,28 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    startup::run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,13 +1,10 @@
-use std::net::TcpListener;
-
 use once_cell::sync::Lazy;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Executor, PgPool};
 
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup;
+use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -33,41 +30,26 @@ pub async fn spawn_app() -> TestApp {
     // All other invocations will instead skip execution.
     Lazy::force(&TRACING);
 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = {
+        let mut config = get_configuration().expect("Failed to read configuration");
+        config.database.database_name = Uuid::new_v4().to_string();
+        config.application.port = 0;
 
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
+        config
+    };
 
-    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configure_database(&configuration.database).await;
 
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application");
 
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    let address = format!("http://127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
 
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    let db_pool = get_connection_pool(&configuration.database);
 
-    let connection_pool = configure_database(&configuration.database).await;
-
-    let server = startup::run(listener, connection_pool.clone(), email_client)
-        .expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-
-    TestApp {
-        address,
-        db_pool: connection_pool,
-    }
+    TestApp { address, db_pool }
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
